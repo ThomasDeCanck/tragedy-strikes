dc66511d52505bd59602a44d54640adcfa8d7d87
Thomas <thomas.decanck@provilion.be>
2026-03-17 13:35:54 +0100

    oef 3

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import pygame
-#e
+from pygame.examples.eventlist import virtual_x
+
 pygame.init()
 
-screen = pygame.display.set_mode((640, 360))
+screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Game')
 
-clock = pygame.time.Clock()  # ✅ create clock here
+clock = pygame.time.Clock()
 
 x = 300
 y = 200
+radius = 50
+
+
+vx = 3
 
 running = True
 while running:
-    clock.tick(60)
+    clock.tick(120)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    x += 1
+    x += vx
+    if x + radius >= 1280:
+        vx = -vx
+
+    if x - radius <= 0:
+        vx = -vx
 
     screen.fill((0, 106, 78))
     pygame.draw.circle(screen, (205, 2, 3), (x, y), 50)
